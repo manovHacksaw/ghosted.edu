33dcbaab00cd86df243e5b82288934fc11475f47
Manov <manovmandal@gmail.com>
2025-03-25 16:33:32 +0530

    deployement fix 9

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,55 +1,55 @@
-import { NextResponse } from "next/server";
-import prisma from "@/lib/prismaclient/prisma";
+// import { NextResponse } from "next/server";
+// import prisma from "@/lib/prismaclient/prisma";
 
-export async function POST(request: Request) {
-  try {
-    const body = await request.json();
-    const { OCId } = body;
+// export async function POST(request: Request) {
+//   try {
+//     const body = await request.json();
+//     const { OCId } = body;
 
-    if (!OCId) {
-      return NextResponse.json({ error: "OCId is required" }, { status: 400 });
-    }
+//     if (!OCId) {
+//       return NextResponse.json({ error: "OCId is required" }, { status: 400 });
+//     }
 
-    // Check if user exists
+//     // Check if user exists
 
-    const existingUser = await prisma.user.findUnique({
-      where: { OCId },
-    });
+//     const existingUser = await prisma.user.findUnique({
+//       where: { OCId },
+//     });
 
-    if (!existingUser) {
-      const newUser = await prisma.user.create({
-        data: { 
-          OCId,
-          isOnboarded: false 
-        },
-      });
+//     if (!existingUser) {
+//       const newUser = await prisma.user.create({
+//         data: { 
+//           OCId,
+//           isOnboarded: false 
+//         },
+//       });
 
-      return NextResponse.json(
-        {
-          user: newUser,
-          isFirstTimeLogin: true,
-          message: "User created successfully",
-        },
-        { status: 201 }
-      );
-    } else {
-      return NextResponse.json(
-        {
-          user: existingUser,
-          isFirstTimeLogin: false,
-          message: "User logged in successfully",
-        },
-        { status: 200 }
-      );
-    }
-  } catch (error: any) {
-    console.error("Login processing error:", error);
-    return NextResponse.json(
-      {
-        error: "Failed to process login",
-        details: error.message,
-      },
-      { status: 500 }
-    );
-  }
-}
+//       return NextResponse.json(
+//         {
+//           user: newUser,
+//           isFirstTimeLogin: true,
+//           message: "User created successfully",
+//         },
+//         { status: 201 }
+//       );
+//     } else {
+//       return NextResponse.json(
+//         {
+//           user: existingUser,
+//           isFirstTimeLogin: false,
+//           message: "User logged in successfully",
+//         },
+//         { status: 200 }
+//       );
+//     }
+//   } catch (error: any) {
+//     console.error("Login processing error:", error);
+//     return NextResponse.json(
+//       {
+//         error: "Failed to process login",
+//         details: error.message,
+//       },
+//       { status: 500 }
+//     );
+//   }
+// }
