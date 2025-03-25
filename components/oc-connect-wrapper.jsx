83a8665af6c8cc326d972b20d0189f74ccf0832b
Manov <manovmandal@gmail.com>
2025-03-25 15:25:59 +0530

    fixes 2

---
+++ components/oc-connect-wrapper.jsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { OCConnect } from '@opencampus/ocid-connect-js';
+
+
+
+export default function OCConnectWrapper({ children, opts, sandboxMode }) {
+  return (
+    <OCConnect opts={opts} sandboxMode={sandboxMode}>
+      {children}
+    </OCConnect>
+  );
+}
